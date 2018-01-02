fd4185ce74e619e22bd2fc92189763f7227e3a5e
Dmitrii Shesterkin <dfb@yandex.ru>
2018-01-02 01:03:02 -0400

    + Script reads from the zip file directly

--- json2gpx.py
+++ json2gpx.py
@@ -9,6 +9,7 @@ import json
 import sys
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
+import zipfile
 
 def Add( s, n ):
     if n:
@@ -17,8 +18,20 @@ def Add( s, n ):
     
     return s
 
-with open(sys.argv[1]) as json_file:
-    src = json.load(json_file)
+if len(sys.argv) >= 3:
+    inFile = sys.argv[1]
+    outFile = sys.argv[2]
+else:
+    inFile = '/storage/emulated/0/Download/takeout.zip'
+    outFile = '/storage/emulated/0/Android/data/net.osmand.plus/files/tracks/import/GoogleMapsExport.gpx'
+
+if inFile.endswith('.zip'):
+    archive = zipfile.ZipFile(inFile, 'r')
+    contents = archive.read(u'Takeout/Карты (ваши отзывы и места)/Сохраненные места.json')
+    src = json.loads(contents)
+else:
+    with open(inFile) as json_file:
+        src = json.load(json_file)
 
 root = ET.Element('gpx', attrib={'version':'1.1'})
 tree = ET.ElementTree(root)
@@ -45,9 +58,10 @@ for f in src["features"]:
     desc = Add(desc, props.get("Updated"))
     ET.SubElement(wpt, 'desc').text = desc.strip()
 
-
 #tree.write(sys.argv[2], encoding="UTF-8", xml_declaration=True)
 
 xmlstr = minidom.parseString(ET.tostring(root)).toprettyxml(indent="   ")
-with open(sys.argv[2], "w") as f:
+with open(outFile, "w") as f:
     f.write(xmlstr.encode('utf-8'))
+
+print( "Converted {} elements".format(len(root.getchildren())) )
